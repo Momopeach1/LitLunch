208406dc5b44b4f6ee9550a5f76f01c3f3b138be
Angel Checo <angel.r.checo@gmail.com>
2019-05-01 09:25:26 -0400

    Updated UI

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react';
 import './App.css';
-import EventCard from './components/EventCard';
+import Events from './components/Events';
 import NaviBar from './components/NaviBar';
+//import { Route, Switch } from 'react-router-dom';
+// <Switch>
+//   <Route exact path="/" component={JoinEvents} />
+//   <Route exact path="/create" component={CreateEvent} />
+//   <Route exact path="/settings" component={Settings} />
+//   <Route exact path="/about" component={About} />
+//   <Route exact path="/profile" component={Profile} />
+// </Switch>
+
+
+
 
 class App extends Component {
+
+
   render() {
     return (
       <div>
         <NaviBar/>
-        <EventCard/>
+        <div className = "my_info_banner"><div id = "location_icon"></div><p style = {{"display": "inline-block","vertical-align": "top;"}}><i>695 Park Ave, New York, NY 10065</i></p></div>
+        <div className = "view-wrap">
+        	<div className = "filter_wrap"><div className = "filter_content"></div></div>
+	        <div className = "events_wrap">
+	      	  <Events/>
+	        </div>
+        </div>
       </div>
 
     );
