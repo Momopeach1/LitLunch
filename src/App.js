8820b25339f2ecb9834bee1232bf81a9e82e04be
Mohammed Elsayed <Mohammed.Elsayed41@myhunter.cuny.edu>
2019-04-30 22:13:02 -0400

    Event Card and NaviBar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import EventCard from './components/EventCard';
+import NaviBar from './components/NaviBar';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <EventCard/>
-        <EventCard/>
+        <NaviBar/>
         <EventCard/>
       </div>
 
--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -1,56 +1,28 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+    
+import React from 'react'
+import { Card, Button, Image } from 'semantic-ui-react'
 
-const styles = {
-  card: {
-    maxWidth: 345,
-  },
-  media: {
-    height: 140,
-  },
-};
+const EventCard = () => (
+  <Card>
+        <Card.Content>
+          <Image size='small' rounded src="https://codepo8.github.io/canvas-images-and-pixels/img/horse.png" />
+          <Card.Header>Momo</Card.Header>
+          <Card.Meta>resturant name</Card.Meta>
+          <Card.Description>
+            Momo wants to Have lunch at <strong>Resturant name</strong>
+          </Card.Description>
+        </Card.Content>
+        <Card.Content extra>
+          <div className='ui two buttons'>
+            <Button basic color='green'>
+              Join
+            </Button>
+            <Button basic color='blue'>
+              Resturant Info
+            </Button>
+          </div>
+        </Card.Content>
+      </Card>
+)
 
-function EventCard(props) {
-  const { classes } = props;
-  return (
-    <Card className={classes.card}>
-      <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          image="https://codepo8.github.io/canvas-images-and-pixels/img/horse.png"
-          title="whoknowstbh"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            Momo
-          </Typography>
-          <Typography component="p">
-            Maybe name of resturant or info on Momo?
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-          Join
-        </Button>
-        <Button size="small" color="primary">
-          resturant info
-        </Button>
-      </CardActions>
-    </Card>
-  );
-}
-
-EventCard.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
-
-export default withStyles(styles)(EventCard);
+export default EventCard
